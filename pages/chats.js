c3f86c89c2a41df7f74a4e5fc46900e17d27ee10
Umair Rao <umairmuqeem@gmail.com>
2023-09-26 14:22:55 +0500

    Add chat page

--- pages/chats.js
+++ pages/chats.js
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 
-export default function Chats() {
-  return <div className="background">chats</div>;
+import { Context } from "../context";
+
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+
+const ChatEngine = dynamic(() =>
+  import("react-chat-engine").then((module) => module.ChatEngine)
+);
+const MessageFormSocial = dynamic(() =>
+  import("react-chat-engine").then((module) => module.MessageFormSocial)
+);
+
+export default function Home() {
+  const { username, secret } = useContext(Context);
+  const [showChat, setShowChat] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof document !== undefined) {
+      setShowChat(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (username === "" || secret === "") {
+      router.push("/");
+    }
+  }, [username, secret]);
+
+  if (!showChat) return <div />;
+
+  return (
+    <div className="background">
+            <div>
+        <button>LogOut</button>
+      </div>
+      <div className="shadow">
+        <ChatEngine
+          height="calc(100vh - 212px)"
+          projectID="04186725-5175-4398-83bd-2b1e55d428ed"
+          userName={username}
+          userSecret={secret}
+          renderNewMessageForm={() => <MessageFormSocial />}
+        />
+      </div>
+
+    </div>
+  );
 }
